c50d7f9b986bb13c0b3daf8f3e8487addf929492
Ibrahim <ibrahimzerlin@hotmail.com>
2021-06-21 21:34:51 -0500

    state changes done

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,43 +1,37 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
-
 import { render, cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
-
 import Nav from '..';
 
-
-// This will ensure that after each test, we won't have any leftover memory data that could give us false results
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-        render(<Nav />);
-    });
+  it('renders', () => {
+    render(<Nav />);
+  });
 
-    // snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
 
-        expect(asFragment()).toMatchSnapshot();
-    });
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
-// To test if the camera emoji is visible
 describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav />);
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
 
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
 })
 
-// we'll check to see if some of our links are visible
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-        const { getByTestId } = render(<Nav />);
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
-    });
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
